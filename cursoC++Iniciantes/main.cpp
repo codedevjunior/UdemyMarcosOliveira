e390a1b00f5a69857c52f005767c6ab940116362
Vilson Jesuino <code.devjunior@outlook.com.br>
2021-06-23 20:43:20 -0300

    iniciando o curso de C++ para iniciantes

---
+++ cursoC++Iniciantes/main.cpp
@@ -0,0 +1,15 @@
+// meu primeiro programa
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Meu primeiro programa em C++" << '\n'; // Isso será impressa na tela
+    return 0;
+}
+
+/* Outro
+ * tipo de
+ * comentarios
+ **/
---
+++ cursoC++Iniciantes/variaveis.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x = 10;
+    char letra = 'Aa';
+
+    cout << "O valor de x é: " << x << " e letra é: " << letra << endl;
+    return 0;
+}
